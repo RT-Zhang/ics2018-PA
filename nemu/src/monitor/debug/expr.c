874f41533584c223e998ae7435fa61d4a059341d
tracer-ics2017 <tracer@njuics.org>
2020-03-11 23:32:48 +0800

    >  compile 1713434 zhang Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  23:32:48 up  8:33,  1 user,  load average: 0.01, 0.04, 0.00 8e2b385366759df0746b05e09659429448a692e8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -267,14 +267,14 @@ uint32_t expr(char *e, bool *success) {
         *success = false;
         return 0;
     }
-    for (int i = 0; i < 0; i++) {
+    for (int i = 0; i < nr_token; i++) {
         int typeBefore = tokens[i - 1].type;
         if (i == 0 || (typeBefore != TK_NUMBER && typeBefore != ')'
                        && typeBefore != TK_HEX && typeBefore != TK_REG)) {
             if (tokens[i].type == '*')
                 tokens[i].type = TK_DEREF;
-            if (tokens[i].type == '-');
-            tokens[i].type = TK_NEGATIVE;
+            if (tokens[i].type == '-')
+                tokens[i].type = TK_NEGATIVE;
         }
     }
     *success = true;
