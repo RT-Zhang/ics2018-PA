9626eb259f2e145fd8be51d9c786ee336dc032d5
tracer-ics2017 <tracer@njuics.org>
2020-03-11 16:58:20 +0800

    >  compile 1713434 zhang Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  16:58:20 up  1:58,  1 user,  load average: 0.05, 0.03, 0.00 7777d93087f3ece3c56aea5dcae6bb6bb8036d73

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,11 +5,11 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
 
 enum {
     TK_NOTYPE = 256,
     TK_NUMBER
-
         /* TODO: Add more token types */
 };
 
@@ -108,12 +108,84 @@ static bool make_token(char *e) {
             return false;
         }
     }
-    int j;
-    printf("token expression:\n");
-    for (j = 0; j < nr_token; j++) {
-        printf("type: %d, str: %s\n", tokens[j].type, tokens[j].str);
-    }
     return true;
+}
+
+bool check_parentheses(int p, int q) {
+    if (p >= q) {
+        printf("error: p>=q in check_parentheses\n");
+        return false;
+    }
+    if (tokens[p].type != '(' || tokens[q].type != ')')
+        return false;
+
+    int count = 0;  // number of unmatched left parentheses
+    for (int curr = p + 1; curr < q; curr++) {
+        if (tokens[curr].type == '(')
+            count++;
+        if (tokens[curr].type == ')') {
+            if (count != 0)
+                count--;
+            else
+                return false;  // exists unmatched right
+        }
+    }
+    if (count == 0)
+        return true;
+    else
+        return false;  // exists unmatched left
+}
+
+int findDominantOp(int p, int q) {
+    int pos[2] = {-1, -1};  // rightmost +-, rightmost */
+    int level = 0;
+
+    for (int i = 0; i < nr_token; i++) {
+        if ((tokens[i].type != '+') && (tokens[i].type != '-')
+            && (tokens[i].type != '*') && (tokens[i].type != '/'))
+            continue;
+        if (tokens[i].type == '(')
+            level++;
+        if (tokens[i].type == ')')
+            level--;
+        if (level == 0) {
+            if ((tokens[i].type == '+') && (tokens[i].type == '-'))
+                pos[0] = i;
+            if ((tokens[i].type == '*') && (tokens[i].type == '/'))
+                pos[1] = i;
+        }
+    }
+    if (pos[0] != -1)
+        return pos[0];
+    else 
+        return pos[1];
+}
+
+int eval(int p, int q) {
+    if (p > q) {
+        printf("error! p > q in eval");
+        assert(0);
+    } else if (p == q) {
+        return atoi(tokens[p].str);
+    } else if (check_parentheses(p, q) == true) {
+        return eval(p + 1, q - 1);
+    } else {
+        int op = findDominantOp(p, q);
+        int val1 = eval(p, op - 1);
+        int val2 = eval(op + 1, q);
+        switch(op) {
+        case '+':
+            return val1 + val2;
+        case '-':
+            return val1 - val2;
+        case '*':
+            return val1 * val2;
+        case '/':
+            return val1 / val2;
+        default:
+            assert(0);
+        }
+    }
 }
 
 uint32_t expr(char *e, bool *success) {
@@ -122,7 +194,6 @@ uint32_t expr(char *e, bool *success) {
         return 0;
     }
 
-    /* TODO: Insert codes to evaluate the expression. */
-
-    return 0;
+    *success = true;
+    return eval(0, nr_token - 1);
 }
