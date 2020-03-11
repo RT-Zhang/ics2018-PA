cc8cbf6e5a26782e903625144d08b1a034e28378
tracer-ics2017 <tracer@njuics.org>
2020-03-11 23:29:52 +0800

    >  compile 1713434 zhang Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  23:29:52 up  8:30,  1 user,  load average: 0.18, 0.07, 0.01 167bfdbedfc275e7a2adfec1e84a7e11fc8260f6

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -101,9 +101,11 @@ static bool make_token(char *e) {
                     switch (rules[i].token_type) {
                     case TK_NUMBER:
                     case TK_HEX:
-                    case TK_REG:
                         strncpy(tokens[nr_token].str, substr_start, substr_len);
                         *(tokens[nr_token].str + substr_len) = '\0';
+                        break;
+                    case TK_REG:
+                        strncpy(tokens[nr_token].str, substr_start + 1, substr_len);
                         break;
                     }
                     nr_token += 1;  // update nr_token
