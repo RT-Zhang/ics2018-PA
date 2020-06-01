4d1bf01b7b498802852be8aafe8a5b916fd65d2c
tracer-ics2017 <tracer@njuics.org>
2020-06-01 15:39:23 +0800

    >  compile 1713434 zhang Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  15:39:23 up  1:04,  1 user,  load average: 0.61, 0.20, 0.06 50a4a5e972a31a1210d651b741e61f166dc82ee0

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -26,7 +26,6 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&t0);
 
   vaddr_t gate_addr = cpu.idtr.base + NO * sizeof(GateDesc);
-  printf("gate_addr=0x%x, base=%d, limit=%d\n", gate_addr, cpu.idtr.base, cpu.idtr.limit);
   assert(gate_addr <= cpu.idtr.base + cpu.idtr.limit);
 
   uint32_t off_15_0 = vaddr_read(gate_addr, 2);
