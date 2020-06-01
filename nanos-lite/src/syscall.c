57860c259bf23ea7f3439e941ed2af327c50ed42
tracer-ics2017 <tracer@njuics.org>
2020-06-01 17:11:32 +0800

    >  run 1713434 zhang Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  17:11:32 up  2:37,  1 user,  load average: 0.16, 0.05, 0.01 c75343cfcb2a6fa98693d2066dddde33cb22ceda

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,33 @@
 #include "common.h"
 #include "syscall.h"
 
+int sys_none() {
+    return 1;
+}
+
+int sys_exit(uintptr_t ah) {
+#ifdef DEBUG
+    printf("In sys_exit, exit code=%d\n", ah);
+#endif
+    return 1;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  case SYS_none:
+      SYSCALL_ARG1(r) = sys_none();
+      break;
+  case SYS_exit:
+      sys_exit(a[1]);
+      break;
+  default:
+      panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
