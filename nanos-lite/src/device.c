0c362c81f826a99796ebefc9a1d5683070e289cf
tracer-ics2017 <tracer@njuics.org>
2020-06-02 01:27:37 +0800

    >  run 1713434 zhang Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  01:27:37 up 10:53,  1 user,  load average: 0.24, 0.10, 0.02 1d7c554ac4fbfd78cf5e1a426a8751f81f297763

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,9 +15,41 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+    strncpy(buf, dispinfo + offset, len);
 }
 
+extern void getScreen(int* p_width, int* p_height);
+
 void fb_write(const void *buf, off_t offset, size_t len) {
+    assert(offset % 4 == 0 && len % 4 == 0);
+    int index, screen_x1, screen_y1, screen_y2;
+    int width = 0, height = 0;
+    getScreen(&width, &height);
+
+    index = offset / 4;
+    screen_y1 = index / width;
+    screen_x1 = index % width;
+
+    index = (offset + len) / 4;
+    screen_y2 = index / width;
+
+    assert(screen_y2 >= screen_y1);
+
+    if (screen_y2 == screen_y1) {
+        _draw_rect(buf, screen_x1, screen_y1, len / 4, 1);
+        return;
+    }
+    int tmpw = width - screen_x1;
+    if (screen_y2 - screen_y1 == 1) {
+        _draw_rect(buf, screen_x1, screen_y1, tmpw, 1);
+        _draw_rect(buf + tmpw * 4, 0, screen_y2, len / 4 - tmpw, 1);
+        return;
+    }
+
+    _draw_rect(buf, screen_x1, screen_y1, tmpw, 1);
+    int tmpy = screen_y2 - screen_y1 - 1;
+    _draw_rect(buf + tmpw * 4, 0, screen_y1 + 1, width, tmpy);
+    _draw_rect(buf + tmpw * 4 + tmpy * width * 4, 0, screen_y2, len / 4 - tmpw - tmpy * width, 1);
 }
 
 void init_device() {
@@ -25,4 +57,7 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  int width = 0, height = 0;
+  getScreen(&width, &height);
+  sprintf(dispinfo, "WIDTH: %d\nHEIGHT: %d\n", width, height);
 }
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -24,6 +24,11 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+  extern void getScreen(int* p_width, int* p_height);
+  int width = 0, height = 0;
+  getScreen(&width, &height);
+  file_table[FD_FB].size = width * height * sizeof(uint32_t);
+  Log("set FD_FB size=%d", file_table[FD_FB].size);
 }
 
 size_t fs_filesz(int fd) {
@@ -62,32 +67,47 @@ int fs_open(const char* filename, int flags, int mode) {
     return -1;
 }
 
+void dispinfo_read(void* buf, off_t offset, size_t len);
+
 ssize_t fs_read(int fd, void* buf, size_t len) {
     assert(fd >= 0 && fd < NR_FILES);
-    if (fd < 3) {
-        Log("arg invalid: fd < 3\n");
+    if (fd < 3 || fd == FD_FB) {
+        Log("arg invalid: fd < 3 || fd == FD_FB\n");
         return 0;
     }
+
     int n = fs_filesz(fd) - get_open_offset(fd);
-    if (n > len) {
+    if (n > len)
         n = len;
-    }
-    ramdisk_read(buf, disk_offset(fd) + get_open_offset(fd), n);
+
+    if (fd == FD_DISPINFO)
+        dispinfo_read(buf, get_open_offset(fd), n);
+    else
+        ramdisk_read(buf, disk_offset(fd) + get_open_offset(fd), n);
+
     set_open_offset(fd, get_open_offset(fd) + n);
     return n;
 }
 
+void fb_write(const void *buf, off_t offset, size_t len);
+
 ssize_t fs_write(int fd, void* buf, size_t len) {
     assert(fd >= 0 && fd < NR_FILES);
-    if (fd < 3) {
-        Log("arg invalid: fd < 3\n");
+
+    if (fd < 3 || fd == FD_FB) {
+        Log("arg invalid: fd < 3 || fd == FD_FB\n");
         return 0;
     }
+
     int n = fs_filesz(fd) - get_open_offset(fd);
-    if (n > len) {
+    if (n > len)
         n = len;
-    }
-    ramdisk_write(buf, disk_offset(fd) + get_open_offset(fd), n);
+
+    if (fd == FD_FB)
+        fb_write(buf, get_open_offset(fd), n);
+    else
+        ramdisk_write(buf, disk_offset(fd) + get_open_offset(fd), n);
+
     set_open_offset(fd, get_open_offset(fd) + n);
     return n;
 }
