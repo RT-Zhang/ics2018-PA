d9d2b0a2de8c53b5a01abc7603262f7a786ec6e7
tracer-ics2017 <tracer@njuics.org>
2020-06-11 01:06:18 +0800

    >  run 1713434 zhang Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 i686 i686 GNU/Linux  01:06:18 up  8:58,  1 user,  load average: 0.21, 0.12, 0.13 c85bda27e2e4e37b565a7ae4d7397427eb01df0

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,6 +21,12 @@ size_t events_read(void *buf, size_t len) {
   else
       sprintf(str, "t %d\n", _uptime());
 
+  if (down && key == _KEY_F12) {
+    extern void switch_current_game();
+    switch_current_game();
+    Log("key down: _KEY_F12, switch current game");
+  }
+
   if (strlen(str) <= len) {
       strncpy((char*)buf, str, strlen(str));
       return strlen(str);
