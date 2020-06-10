5d4f050d305bc54d2b13667b5fd0c0d4cf6f5fa9
tracer-ics2017 <tracer@njuics.org>
2020-06-11 00:24:35 +0800

    >  run 1713434 zhang Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 i686 i686 GNU/Linux  00:24:35 up  8:16,  1 user,  load average: 0.14, 0.15, 0.50 9b6b49ac233572ae323dd28c14c8191411752040

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,13 +27,24 @@ void load_prog(const char *filename) {
 }
 
 _RegSet* schedule(_RegSet *prev) {
-  if (current != NULL) {
+  if (current != NULL)
     current->tf = prev;
-  }
+  else
+    current = &pcb[0];
 
-  //current = &pcb[0];
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  Log("ptr=0x%x\n", (uint32_t)current->as.ptr);
+  static int num = 0;
+  static const int frequency = 1000;
+
+  if (current == &pcb[0])
+    num++;
+  else
+    current = &pcb[0];
+
+  if (num == frequency) {
+    current = &pcb[1];
+    num = 0;
+  }
+  
   _switch(&current->as);
   return current->tf;
 }
