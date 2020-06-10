56117e79b16e4fc4c1ebd8ef8d17e160199ba291
tracer-ics2017 <tracer@njuics.org>
2020-06-10 17:10:29 +0800

    >  run 1713434 zhang Linux ubuntu 4.15.0-106-generic #107~16.04.1-Ubuntu SMP Thu Jun 4 15:39:45 UTC 2020 i686 i686 i686 GNU/Linux  17:10:29 up  1:02,  1 user,  load average: 0.38, 0.19, 0.12 1734f8d6cb7f952c4b729928cb7ecd905516d44f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "fs.h"
+#include "memory.h"  // added in pa4-1
 
 #define DEFAULT_ENTRY ((void *)0x8048000)
 
@@ -11,7 +12,19 @@ extern void ramdisk_read(void *buf, off_t* offset, size_t len);
 uintptr_t loader(_Protect *as, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   Log("filename=%s, fd=%d", filename, fd);
-  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+
+  int size = fs_filesz(fd);
+  int ppnum = size / PGSIZE;
+  if (size % PGSIZE != 0)
+    ppnum++;
+  void* pa = NULL;
+  void* va = DEFAULT_ENTRY;
+  for (int i = 0; i < ppnum; i++) {
+    pa = new_page();
+    _map(as, va, pa);
+    fs_read(fd, pa, PGSIZE);
+    va += PGSIZE;
+  }
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
